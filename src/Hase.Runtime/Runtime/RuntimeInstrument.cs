279892825e01a27b53add01909004a93d6451b68
andiepp <a.eppinger@gmx.net>
2026-07-09 11:55:57 +0200

    Add initial runtime instrument

---
+++ src/Hase.Runtime/Runtime/RuntimeInstrument.cs
@@ -0,0 +1,20 @@
+﻿using Hase.Core.Domain.Instruments;
+
+namespace Hase.Runtime.Runtime;
+
+/// <summary>
+/// Runtime representation of an instrument.
+/// It references the immutable instrument descriptor.
+/// </summary>
+public sealed class RuntimeInstrument
+{
+    public RuntimeInstrument(InstrumentDescriptor descriptor)
+    {
+        Descriptor = descriptor ?? throw new ArgumentNullException(nameof(descriptor));
+    }
+
+    /// <summary>
+    /// Static descriptor of this instrument.
+    /// </summary>
+    public InstrumentDescriptor Descriptor { get; }
+}
