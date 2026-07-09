a09530bd1c284c0dacd0419ffb978470ac81302f
andiepp <a.eppinger@gmx.net>
2026-07-09 20:34:34 +0200

    Add discovery service abstraction

---
+++ src/Hase.Runtime/Services/IDiscoveryService.cs
@@ -0,0 +1,13 @@
+﻿using Hase.Runtime.Runtime;
+
+namespace Hase.Runtime.Services;
+
+/// <summary>
+/// Discovers endpoints and adds them to a runtime context.
+/// </summary>
+public interface IDiscoveryService
+{
+    Task DiscoverAsync(
+        RuntimeContext context,
+        CancellationToken cancellationToken = default);
+}
