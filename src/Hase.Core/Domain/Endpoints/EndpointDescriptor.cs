93da8e97a5c37c34ddc9fde6724ee536e3f27727
andiepp <a.eppinger@gmx.net>
2026-07-09 13:46:02 +0200

    Add endpoint descriptor

---
+++ src/Hase.Core/Domain/Endpoints/EndpointDescriptor.cs
@@ -0,0 +1,23 @@
+﻿using Hase.Core.Domain.Identity;
+using Hase.Core.Domain.Instruments;
+
+namespace Hase.Core.Domain.Endpoints;
+
+public sealed record EndpointDescriptor
+{
+    public EndpointDescriptor(
+        EndpointId id,
+        IEnumerable<InstrumentDescriptor>? instruments = null)
+    {
+        Id = id ?? throw new ArgumentNullException(nameof(id));
+
+        Instruments = (instruments ?? Enumerable.Empty<InstrumentDescriptor>())
+            .ToArray();
+    }
+
+    public EndpointId Id { get; }
+
+    public EndpointMetadata Metadata { get; init; } = new();
+
+    public IReadOnlyList<InstrumentDescriptor> Instruments { get; }
+}
---
+++ src/Hase.Core/Domain/Endpoints/EndpointMetadata.cs
@@ -0,0 +1,8 @@
+﻿namespace Hase.Core.Domain.Endpoints;
+
+public sealed record EndpointMetadata
+{
+    public string? DisplayName { get; init; }
+
+    public string? Description { get; init; }
+}
