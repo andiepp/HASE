d3c0895b37583279677fadb6c36c82c49515e6cd
andiepp <a.eppinger@gmx.net>
2026-07-09 16:40:41 +0200

    Add runtime event model

---
+++ src/Hase.Runtime/Runtime/RuntimeEvent.cs
@@ -0,0 +1,13 @@
+﻿using Hase.Core.Domain.Events;
+
+namespace Hase.Runtime.Runtime;
+
+public sealed class RuntimeEvent
+{
+    public RuntimeEvent(EventDescriptor descriptor)
+    {
+        Descriptor = descriptor ?? throw new ArgumentNullException(nameof(descriptor));
+    }
+
+    public EventDescriptor Descriptor { get; }
+}
--- src/Hase.Runtime/Runtime/RuntimeInstrument.cs
+++ src/Hase.Runtime/Runtime/RuntimeInstrument.cs
@@ -1,6 +1,7 @@
 ﻿using Hase.Core.Domain.Instruments;
 using Hase.Core.Domain.Properties;
 using Hase.Core.Domain.Commands;
+using Hase.Core.Domain.Events;
 
 namespace Hase.Runtime.Runtime;
 
@@ -21,6 +22,11 @@ public sealed class RuntimeInstrument
         {
             _commands.Add(new RuntimeCommand(command));
         }
+
+        foreach (var eventDescriptor in descriptor.Interface.Events)
+        {
+            _events.Add(new RuntimeEvent(eventDescriptor));
+        }
     }
 
     public InstrumentDescriptor Descriptor { get; }
@@ -30,6 +36,9 @@ public sealed class RuntimeInstrument
     private readonly List<RuntimeCommand> _commands = [];
     public IReadOnlyList<RuntimeCommand> Commands => _commands;
 
+    private readonly List<RuntimeEvent> _events = [];
+    public IReadOnlyList<RuntimeEvent> Events => _events;
+
     public RuntimeProperty? FindProperty(DescriptorPath path)
     {
         ArgumentNullException.ThrowIfNull(path);
@@ -37,12 +46,21 @@ public sealed class RuntimeInstrument
         return _properties.FirstOrDefault(
             property => property.Descriptor.Path == path);
     }
+
     public RuntimeCommand? FindCommand(DescriptorPath path)
     {
         ArgumentNullException.ThrowIfNull(path);
 
         return _commands.FirstOrDefault(
             command => command.Descriptor.Path == path);
+    }
+
+    public RuntimeEvent? FindEvent(DescriptorPath path)
+    {
+        ArgumentNullException.ThrowIfNull(path);
+
+        return _events.FirstOrDefault(
+            runtimeEvent => runtimeEvent.Descriptor.Path == path);
     }
 
 }
