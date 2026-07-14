a247cc59e5508a6165a4a6fefc56042a87355900
andiepp <a.eppinger@gmx.net>
2026-07-14 08:02:46 +0200

    Add TCP transport endpoint options

---
+++ src/Hase.Transport/Tcp/TcpTransportOptions.cs
@@ -0,0 +1,55 @@
+﻿namespace Hase.Transport.Tcp;
+
+/// <summary>
+/// Defines the remote endpoint used by a TCP transport connection.
+/// </summary>
+public sealed class TcpTransportOptions
+{
+    /// <summary>
+    /// Initializes TCP transport options.
+    /// </summary>
+    /// <param name="host">
+    /// IPv4 address, IPv6 address, or DNS host name of the remote endpoint.
+    /// </param>
+    /// <param name="port">
+    /// TCP port of the remote endpoint.
+    /// </param>
+    public TcpTransportOptions(
+        string host,
+        int port)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(
+            host);
+
+        if (port is < 1 or > 65535)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(port),
+                port,
+                "The TCP port must be between 1 and 65535.");
+        }
+
+        Host =
+            host;
+
+        Port =
+            port;
+    }
+
+    /// <summary>
+    /// Gets the IPv4 address, IPv6 address, or DNS host name
+    /// of the remote endpoint.
+    /// </summary>
+    public string Host
+    {
+        get;
+    }
+
+    /// <summary>
+    /// Gets the TCP port of the remote endpoint.
+    /// </summary>
+    public int Port
+    {
+        get;
+    }
+}
---
+++ tests/Hase.Transport.Tests/TcpTransportOptionsTests.cs
@@ -0,0 +1,114 @@
+﻿using Hase.Transport.Tcp;
+
+namespace Hase.Transport.Tests;
+
+public sealed class TcpTransportOptionsTests
+{
+    [Fact]
+    public void Constructor_ShouldStoreHostAndPort()
+    {
+        // Arrange
+        const string expectedHost =
+            "192.168.1.42";
+
+        const int expectedPort =
+            5000;
+
+        // Act
+        var options =
+            new TcpTransportOptions(
+                expectedHost,
+                expectedPort);
+
+        // Assert
+        Assert.Equal(
+            expectedHost,
+            options.Host);
+
+        Assert.Equal(
+            expectedPort,
+            options.Port);
+    }
+
+    [Fact]
+    public void Constructor_NullHost_ShouldThrow()
+    {
+        // Act
+        void Act()
+        {
+            _ = new TcpTransportOptions(
+                null!,
+                5000);
+        }
+
+        // Assert
+        Assert.Throws<
+            ArgumentNullException>(
+                Act);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("\t")]
+    public void Constructor_EmptyOrWhitespaceHost_ShouldThrow(
+        string host)
+    {
+        // Act
+        void Act()
+        {
+            _ = new TcpTransportOptions(
+                host,
+                5000);
+        }
+
+        // Assert
+        Assert.Throws<
+            ArgumentException>(
+                Act);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(65536)]
+    public void Constructor_InvalidPort_ShouldThrow(
+        int port)
+    {
+        // Act
+        void Act()
+        {
+            _ = new TcpTransportOptions(
+                "localhost",
+                port);
+        }
+
+        // Assert
+        ArgumentOutOfRangeException exception =
+            Assert.Throws<
+                ArgumentOutOfRangeException>(
+                    Act);
+
+        Assert.Equal(
+            "port",
+            exception.ParamName);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(65535)]
+    public void Constructor_BoundaryPort_ShouldSucceed(
+        int port)
+    {
+        // Act
+        var options =
+            new TcpTransportOptions(
+                "localhost",
+                port);
+
+        // Assert
+        Assert.Equal(
+            port,
+            options.Port);
+    }
+}
