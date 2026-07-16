0ae60778845f85cd3c5ad8f3b77a46395f3ac441
andiepp <a.eppinger@gmx.net>
2026-07-16 11:10:03 +0200

    Add runtime transport test project

---
+++ tests/Hase.Runtime.Transport.Tests/BridgeSmokeTests.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace Hase.Runtime.Transport.Tests;
+
+public sealed class BridgeSmokeTests
+{
+    [Fact]
+    public void TestProjectIsOperational()
+    {
+        Assert.True(true);
+    }
+}
