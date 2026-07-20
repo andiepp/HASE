b4b54995f9d8b848829dc4771c45f17c22a6f2b9
andiepp <a.eppinger@gmx.net>
2026-07-20 17:21:01 +0200

    Add System IO Ports settings mapping

---
+++ src/Hase.Transport/Serial/SystemIoPortsSerialSettingsMapper.cs
@@ -0,0 +1,84 @@
+﻿using System.IO.Ports;
+
+namespace Hase.Transport.Serial;
+
+/// <summary>
+/// Maps transport-independent serial settings to System.IO.Ports values.
+/// </summary>
+internal static class SystemIoPortsSerialSettingsMapper
+{
+    public static Parity MapParity(
+        SerialParity parity)
+    {
+        return parity switch
+        {
+            SerialParity.None =>
+                Parity.None,
+
+            SerialParity.Odd =>
+                Parity.Odd,
+
+            SerialParity.Even =>
+                Parity.Even,
+
+            SerialParity.Mark =>
+                Parity.Mark,
+
+            SerialParity.Space =>
+                Parity.Space,
+
+            _ =>
+                throw new ArgumentOutOfRangeException(
+                    nameof(parity),
+                    parity,
+                    "The serial parity value is not supported.")
+        };
+    }
+
+    public static StopBits MapStopBits(
+        SerialStopBits stopBits)
+    {
+        return stopBits switch
+        {
+            SerialStopBits.One =>
+                StopBits.One,
+
+            SerialStopBits.OnePointFive =>
+                StopBits.OnePointFive,
+
+            SerialStopBits.Two =>
+                StopBits.Two,
+
+            _ =>
+                throw new ArgumentOutOfRangeException(
+                    nameof(stopBits),
+                    stopBits,
+                    "The serial stop-bit value is not supported.")
+        };
+    }
+
+    public static Handshake MapHandshake(
+        SerialHandshake handshake)
+    {
+        return handshake switch
+        {
+            SerialHandshake.None =>
+                Handshake.None,
+
+            SerialHandshake.XOnXOff =>
+                Handshake.XOnXOff,
+
+            SerialHandshake.RequestToSend =>
+                Handshake.RequestToSend,
+
+            SerialHandshake.RequestToSendXOnXOff =>
+                Handshake.RequestToSendXOnXOff,
+
+            _ =>
+                throw new ArgumentOutOfRangeException(
+                    nameof(handshake),
+                    handshake,
+                    "The serial handshake value is not supported.")
+        };
+    }
+}
---
+++ tests/Hase.Transport.Tests/SystemIoPortsSerialSettingsMapperTests.cs
@@ -0,0 +1,123 @@
+﻿using System.IO.Ports;
+using Hase.Transport.Serial;
+
+namespace Hase.Transport.Tests;
+
+public sealed class SystemIoPortsSerialSettingsMapperTests
+{
+    [Theory]
+    [InlineData(SerialParity.None, Parity.None)]
+    [InlineData(SerialParity.Odd, Parity.Odd)]
+    [InlineData(SerialParity.Even, Parity.Even)]
+    [InlineData(SerialParity.Mark, Parity.Mark)]
+    [InlineData(SerialParity.Space, Parity.Space)]
+    public void MapParity_DefinedValue_ShouldMapExactly(
+        SerialParity source,
+        Parity expected)
+    {
+        Parity actual =
+            SystemIoPortsSerialSettingsMapper
+                .MapParity(
+                    source);
+
+        Assert.Equal(
+            expected,
+            actual);
+    }
+
+    [Fact]
+    public void MapParity_UndefinedValue_ShouldThrow()
+    {
+        void Act()
+        {
+            _ = SystemIoPortsSerialSettingsMapper
+                .MapParity(
+                    (SerialParity)99);
+        }
+
+        ArgumentOutOfRangeException exception =
+            Assert.Throws<ArgumentOutOfRangeException>(
+                Act);
+
+        Assert.Equal(
+            "parity",
+            exception.ParamName);
+    }
+
+    [Theory]
+    [InlineData(SerialStopBits.One, StopBits.One)]
+    [InlineData(SerialStopBits.OnePointFive, StopBits.OnePointFive)]
+    [InlineData(SerialStopBits.Two, StopBits.Two)]
+    public void MapStopBits_DefinedValue_ShouldMapExactly(
+        SerialStopBits source,
+        StopBits expected)
+    {
+        StopBits actual =
+            SystemIoPortsSerialSettingsMapper
+                .MapStopBits(
+                    source);
+
+        Assert.Equal(
+            expected,
+            actual);
+    }
+
+    [Fact]
+    public void MapStopBits_UndefinedValue_ShouldThrow()
+    {
+        void Act()
+        {
+            _ = SystemIoPortsSerialSettingsMapper
+                .MapStopBits(
+                    (SerialStopBits)99);
+        }
+
+        ArgumentOutOfRangeException exception =
+            Assert.Throws<ArgumentOutOfRangeException>(
+                Act);
+
+        Assert.Equal(
+            "stopBits",
+            exception.ParamName);
+    }
+
+    [Theory]
+    [InlineData(SerialHandshake.None, Handshake.None)]
+    [InlineData(SerialHandshake.XOnXOff, Handshake.XOnXOff)]
+    [InlineData(SerialHandshake.RequestToSend, Handshake.RequestToSend)]
+    [InlineData(
+        SerialHandshake.RequestToSendXOnXOff,
+        Handshake.RequestToSendXOnXOff)]
+    public void MapHandshake_DefinedValue_ShouldMapExactly(
+        SerialHandshake source,
+        Handshake expected)
+    {
+        Handshake actual =
+            SystemIoPortsSerialSettingsMapper
+                .MapHandshake(
+                    source);
+
+        Assert.Equal(
+            expected,
+            actual);
+    }
+
+    [Fact]
+    public void MapHandshake_UndefinedValue_ShouldThrow()
+    {
+        void Act()
+        {
+            _ = SystemIoPortsSerialSettingsMapper
+                .MapHandshake(
+                    (SerialHandshake)99);
+        }
+
+        ArgumentOutOfRangeException exception =
+            Assert.Throws<ArgumentOutOfRangeException>(
+                Act);
+
+        Assert.Equal(
+            "handshake",
+            exception.ParamName);
+    }
+}
